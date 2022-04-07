1395de2b2c038e5b89004b1a11d566bb551d512b
Igor Rodrigues <igorr2693@gmail.com>
2022-04-07 00:23:41 -0300

    Excluindo eventos

--- routes/eventRoutes.js
+++ routes/eventRoutes.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const checkAuth = require('../helpers/auth').checkAuth
 const EventController = require('../controllers/EventController')
 
+router.get('/', EventController.showAllEvents)
+
 router.get('/', checkAuth, EventController.showAllEvents)
 router.get('/showEvent', checkAuth, EventController.showEvent)
 
 router.get('/add', checkAuth, EventController.createEvent)
 router.post('/add', checkAuth, EventController.createEventSave)
 
-
+router.post('/remove', checkAuth, EventController.removeEvent)
 
 
 module.exports = router
